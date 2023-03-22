6b58e9a9123c6fce77739d6c8352cf9e61308bac
Roshan Bhatia <roshan@timebyping.com>
2023-03-21 19:17:18 -0700

    locate pods with annotation

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"flag"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
+)
+
+const (
+	annotationKey = "inject-configmap"
 )
 
 var (
@@ -24,6 +34,34 @@ func main() {
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
 
-
 	klog.Infof("Parsed config: %v", cfg)
+
+	clientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	factory := informers.NewSharedInformerFactory(clientset, 30*time.Second)
+	podInformer := factory.Core().V1().Pods().Informer()
+
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			newPod := obj.(*corev1.Pod)
+			if _, ok := newPod.Annotations[annotationKey]; ok {
+				klog.Info("Found new pod with annotation")
+			}
+		},
+		UpdateFunc: func(_, newObj interface{}) {
+			newPod := newObj.(*corev1.Pod)
+
+			if _, ok := newPod.Annotations[annotationKey]; ok {
+				klog.Info("Found updated pod with annotation")
+			}
+		},
+	})
+
+	shouldExit := make(chan struct{})
+	defer close(shouldExit)
+	factory.Start(shouldExit)
+	<-shouldExit
 }
